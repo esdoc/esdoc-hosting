7f7b9aadba38f7c9928590acbb3c4db1dabfb5b3
h13i32maru <h13i32maru@gmail.com>
2015-08-23 14:56:13 +0900

    test: add travis

---
+++ script/install-tool.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# git, sqlite3
+apt-get install git sqlite3
+
+# esdoc plugin
+npm install -g esdoc-es7-plugin esdoc-importpath-plugin
+
+# elasticsearch
+cd elasticsearch
+wget https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-1.7.1.zip
+unzip elasticsearch-1.7.1.zip
+./bin/elasticsearch &
+sleep 1
+curl -XPOST localhost:9200/esdoc -d @mapping.json
+cd ../
+
