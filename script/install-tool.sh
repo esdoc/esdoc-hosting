c708e9731f55cff28d518d5b1b6e6ca8ffc24801
h13i32maru <h13i32maru@gmail.com>
2015-08-23 15:04:00 +0900

    chore: fix nit

--- script/install-tool.sh
+++ script/install-tool.sh
@@ -11,7 +11,7 @@ cd elasticsearch
 wget https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-1.7.1.zip
 unzip elasticsearch-1.7.1.zip
 ./elasticsearch-1.7.1/bin/elasticsearch &
-sleep 1
+sleep 5
 curl -XPOST localhost:9200/esdoc -d @mapping.json
 cd ../
 
