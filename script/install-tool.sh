e1cc7a02f5df627581b44b67686f55f8be485884
h13i32maru <h13i32maru@gmail.com>
2015-08-23 14:58:41 +0900

    chore: fix nit

--- script/install-tool.sh
+++ script/install-tool.sh
@@ -10,7 +10,7 @@ npm install -g esdoc-es7-plugin esdoc-importpath-plugin
 cd elasticsearch
 wget https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-1.7.1.zip
 unzip elasticsearch-1.7.1.zip
-./bin/elasticsearch &
+./elasticsarch-1.7.1/bin/elasticsearch &
 sleep 1
 curl -XPOST localhost:9200/esdoc -d @mapping.json
 cd ../
