a5270cca5d73009c4dd55d540f92a32218ea0700
h13i32maru <h13i32maru@gmail.com>
2015-08-23 15:11:38 +0900

    chore: fix nit

--- script/install-tool.sh
+++ script/install-tool.sh
@@ -4,14 +4,14 @@
 apt-get install git sqlite3
 
 # esdoc plugin
-npm install -g esdoc-es7-plugin esdoc-importpath-plugin
+npm install -g esdoc esdoc-es7-plugin esdoc-importpath-plugin
 
 # elasticsearch
 cd elasticsearch
 wget https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-1.7.1.zip
 unzip elasticsearch-1.7.1.zip
 ./elasticsearch-1.7.1/bin/elasticsearch &
-sleep 5
+sleep 10
 curl -XPOST localhost:9200/esdoc -d @mapping.json
 cd ../
 
